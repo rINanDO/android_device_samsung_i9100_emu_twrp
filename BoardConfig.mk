99724871b2906e2024d72d4ac9db8460dc1b1db5
Dominggoes Isakh <drjisakh@gmail.com>
2017-02-10 21:03:42 +0100

    i9100_emu_twrp: Move TWRP settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,15 +15,3 @@
 #
 
 -include device/samsung/i9100_emu/BoardConfig.mk
-
-# TWRP
-TW_THEME := portrait_mdpi
-TWRP_NEW_THEME := true
-HAVE_SELINUX := true
-TW_MAX_BRIGHTNESS := 255
-TW_INCLUDE_CRYPTO := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_REBOOT_BOOTLOADER := true
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/platform/s5p-tmu/temperature"
-RECOVERY_SDCARD_ON_DATA := true
--- i9100_emu_twrp.mk
+++ i9100_emu_twrp.mk
@@ -22,3 +22,17 @@ LOCAL_PATH := device/samsung/i9100_emu_twrp
 # TWRP Recovery rootdir
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/twrp.fstab:recovery/root/etc/twrp.fstab
+
+# TWRP
+TW_THEME := portrait_mdpi
+#TW_USE_TOOLBOX := true
+TWRP_NEW_THEME := true
+HAVE_SELINUX := true
+TW_MAX_BRIGHTNESS := 255
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_PARTED := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/platform/s5p-tmu/temperature"
+RECOVERY_SDCARD_ON_DATA := true
